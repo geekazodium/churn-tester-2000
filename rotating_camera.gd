5a6057afc95de98b47422eb3ea4759e9b5c63304
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-07 22:11:39 -0500

    enhancement: juiced up camera

---
+++ rotating_camera.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@onready var default_zoom: Vector2 = self.zoom;
+@export var zoom_change: float = .5;
+
+func _ready() -> void:
+	self.ignore_rotation = false;
+
+func _on_reel_input_circle_angular_v_updated(velocity: float) -> void:
+	self.global_rotation_degrees = sqrt(abs(velocity)) * sign(velocity);
+	self.zoom = self.default_zoom * (1 + (1-exp(-abs(velocity))) * self.zoom_change);
+	print(self.global_rotation_degrees);
