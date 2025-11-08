2533ee0e94e0369d3e7bca8b50c97ceef37d4ade
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-07 22:17:49 -0500

    fix: more intuitive rotate direction

--- rotating_camera.gd
+++ rotating_camera.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	self.ignore_rotation = false;
 
 func _on_reel_input_circle_angular_v_updated(velocity: float) -> void:
-	self.global_rotation_degrees = sqrt(abs(velocity)) * sign(velocity);
+	self.global_rotation_degrees = -sqrt(abs(velocity)) * sign(velocity);
 	self.zoom = self.default_zoom * (1 + (1-exp(-abs(velocity) * .2)) * self.zoom_change);
