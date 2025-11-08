06816f2996b65cc9d2286092f13cc85c94890540
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-07 22:29:29 -0500

    feature: setting to switch between units

--- rotation_speed_display.gd
+++ rotation_speed_display.gd
@@ -1,9 +1,19 @@
 extends Label
 
-@export var accuracy: float = 20;
+@export var use_per_sec_toggle: CheckButton;
 
-const RAD_PER_SEC_TO_RPS: float = 1 / (2 * PI); #* 60;
+var use_per_second: bool:
+	get:
+		return use_per_sec_toggle.button_pressed;
+
+const RAD_PER_SEC_TO_RPS: float = 1 / (2 * PI);
 
 func _on_reel_input_circle_angular_v_updated(velocity: float) -> void:
-	var rotation_v: float = roundf(velocity * RAD_PER_SEC_TO_RPS * accuracy) / accuracy;
-	self.text = "rotations per second: %.2f" % [rotation_v];
+	var scale_fac: float = RAD_PER_SEC_TO_RPS * (1 if use_per_second else 60);
+	var rotation_v: float = velocity * scale_fac;
+	var fmt: Array[String];
+	if use_per_second:
+		fmt = ["second", "%.2f"];
+	else:
+		fmt = ["minute", "%.0f"];
+	self.text = ("rotations per %s: %s" % fmt) % [rotation_v];
