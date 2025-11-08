f7a701feb6f82ae559b5d3f1467fb532fb49fae4
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-08 00:48:07 -0500

    feature: restart button

--- main_scene/retry_button.gd
+++ main_scene/retry_button.gd
@@ -1,4 +1,17 @@
 extends Button
 
-func _pressed() -> void:
-	self.get_tree().reload_current_scene();
+@onready var default_text: String = self.text;
+
+@export var hold_time: float = 0;
+var current_hold_time: float = 0;
+
+func _process(delta: float) -> void:
+	if self.button_pressed:
+		if self.current_hold_time >= self.hold_time:
+			self.get_tree().reload_current_scene();
+			return;
+		self.text = "hold" + ".".repeat(round(self.current_hold_time / self.hold_time * 4));
+		self.current_hold_time += delta;
+	else:
+		self.text = self.default_text;
+		self.current_hold_time = 0;
