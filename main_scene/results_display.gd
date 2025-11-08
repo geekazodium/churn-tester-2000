97aa5e51d8679543ede99ef7853cbec6635db3c2
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-08 00:39:11 -0500

    feature: tweaked main scene display

--- main_scene/results_display.gd
+++ main_scene/results_display.gd
@@ -1,5 +1,5 @@
 extends Container
 
 func _on_main_game_ended(rotation_count: float) -> void:
-	$ResultsLabel.text = "YOUR ROTATION COUNT: %.3f" % [rotation_count];
+	$ResultsLabel.text = "YOUR ROTATION COUNT: %.3f in %.0fs" % [rotation_count, 20];
 	self.visible = true;
